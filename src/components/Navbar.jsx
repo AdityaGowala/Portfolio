127233d75087b5c3d7256dd3cf31566f30990bd8
AdityaGowala <adityagowala656@gmail.com>
2025-08-28 11:04:23 +0530

    Resume link fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,13 +68,14 @@ const Navbar = () => {
           </a>
 
           {/* Resume Button */}
-          <a
-            href="/Adityaa_Gowala_resume1.pdf"
-            target="_blank"
-            className="px-4 py-2 bg-green-600 text-white rounded-md font-semibold hover:bg-green-700 transition"
-          >
-            Resume
-          </a>
+         <a
+  href="/Portfolio/Adityaa_Gowala_resume1.pdf"
+  target="_blank"
+  className="px-4 py-2 bg-green-600 text-white rounded-md font-semibold hover:bg-green-700 transition"
+>
+  Resume
+</a>
+
         </div>
 
         {/* Hamburger Icon (Mobile) */}
