f0f96573c268afe7cad83a9aac2617222c7716b7
AdityaGowala <adityagowala656@gmail.com>
2025-10-07 19:21:21 +0530

    e

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -120,7 +120,7 @@ const Navbar = () => {
             </a>
           </div>
           <a
-            href="/My_Resume.pdf"
+            href="/Portfolio/My_Resume.pdf"
             className="block w-full text-center mt-2 px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition"
           >
             Resume
