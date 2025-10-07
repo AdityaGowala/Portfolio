865473abb8590391865c27c19c1a583617d3532a
AdityaGowala <adityagowala656@gmail.com>
2025-10-07 19:07:29 +0530

    updated cv 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
 
           {/* Resume Button */}
          <a
-  href="/Portfolio/Adityaa_Gowala_resume1.pdf"
+  href="/Portfolio/My_Resume.pdf"
   target="_blank"
   className="px-4 py-2 bg-green-600 text-white rounded-md font-semibold hover:bg-green-700 transition"
 >
